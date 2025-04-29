fix logprobs for vllm 0.8.5

tests/common/vllm_test.py
@@ -100,9 +100,11 @@ class BaseTestModelWrapper:
         results = self.model_wrapper.chat(messages)
         self.assertEqual(len(results), self.config.explorer.repeat_times)
         for result in results:
+            input_logprobs = result.logprobs[: result.prompt_length]
             output_logprobs = result.logprobs[result.prompt_length :]
-            self.assertTrue(torch.all(torch.tensor(output_logprobs) != 0))
-        logprobs = self.model_wrapper.logprobs(results[0].tokens)
+            self.assertTrue(torch.all(input_logprobs == 0))
+            self.assertTrue(torch.any(output_logprobs != 0))
+        logprobs = self.model_wrapper.logprobs(results[0].tokens.tolist())
         self.assertEqual(logprobs.shape[0], results[0].tokens.shape[0])
         messages.append(
             {