ee6e9cfcf7dd86afb8066e069fd2c35584bbcaec
pxc <panxuchen.pxc@alibaba-inc.com>
2026-06-09 20:15:26 +0800

    supress vllm warning

--- trinity/common/models/vllm_worker.py
+++ trinity/common/models/vllm_worker.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Custom vLLM Worker."""
+import logging
+
 import ray
 import torch
 import torch.distributed
@@ -10,6 +12,18 @@ from trinity.utils.distributed import init_process_group
 from trinity.utils.log import get_logger
 
 
+def _suppress_layerwise_reload_warnings() -> None:
+    """Silence benign vLLM layerwise reload warnings during weight sync."""
+    try:
+        logger = logging.getLogger(
+            "vllm.model_executor.model_loader.reload.layerwise"
+        )
+        if logger is not None:
+            logger.setLevel(logging.ERROR)
+    except Exception:  # pragma: no cover - best-effort suppression
+        pass
+
+
 class WorkerExtension:
     def apply_patches(self):
         """Apply necessary patches to vLLM."""
@@ -17,6 +31,7 @@ class WorkerExtension:
 
         patch_vllm_moe_model_weight_loader(self.model_runner.model)
         patch_vllm_prompt_logprobs(self.model_runner)
+        _suppress_layerwise_reload_warnings()
 
     def init_process_group(
         self,
