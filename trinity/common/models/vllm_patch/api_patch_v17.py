e105185447ef789fcd4a3f9fa93481dfd0f41196
Xuchen Pan <32844285+pan-x-c@users.noreply.github.com>
2026-06-23 14:28:17 +0800

    Update vLLM to v0.23.0, SGLang to v0.5.13 (#586)

--- trinity/common/models/vllm_patch/api_patch_v17.py
+++ trinity/common/models/vllm_patch/api_patch_v17.py
@@ -17,7 +17,6 @@ from vllm.entrypoints.openai.api_server import (
     validate_api_server_args,
 )
 from vllm.entrypoints.openai.cli_args import make_arg_parser
-from vllm.entrypoints.utils import log_non_default_args
 from vllm.reasoning import ReasoningParserManager
 from vllm.tool_parsers import ToolParserManager
 from vllm.utils.argparse_utils import FlexibleArgumentParser
@@ -33,7 +32,14 @@ def setup_server_in_ray(args, logger):
     ready to serve."""
 
     logger.info("vLLM API server version %s", VLLM_VERSION)
-    log_non_default_args(args)
+    if get_vllm_version() < parse_version("0.23.0"):
+        from vllm.entrypoints.utils import log_non_default_args
+
+        log_non_default_args(args)
+    else:
+        from vllm.entrypoints.serve.utils.api_utils import log_non_default_args
+
+        log_non_default_args(args)
 
     if args.tool_parser_plugin and len(args.tool_parser_plugin) > 3:
         ToolParserManager.import_tool_parser(args.tool_parser_plugin)
